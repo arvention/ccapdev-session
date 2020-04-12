f6d347de7a1dedbe6a89b104b5a223e4980ab682
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-04-12 17:38:52 +0800

    Added root files

---
+++ index.js
@@ -0,0 +1,86 @@
+
+// import module `express`
+const express = require('express');
+
+// import module `hbs`
+const hbs = require('hbs');
+
+// import module `express-session`
+const session = require('express-session');
+
+// import module `mongoose`
+const mongoose = require('mongoose');
+
+// import module `connect-mongo`
+const MongoStore = require('connect-mongo')(session);
+
+// import module `routes` from `./routes/routes.js`
+const routes = require('./routes/routes.js');
+
+// import module `database` from `./model/db.js`
+const db = require('./models/db.js');
+
+const app = express();
+const port = 9090;
+
+// set `hbs` as view engine
+app.set('view engine', 'hbs');
+
+// sets `/views/partials` as folder containing partial hbs files
+hbs.registerPartials(__dirname + '/views/partials');
+
+// parses incoming requests with urlencoded payloads
+app.use(express.urlencoded({extended: true}));
+
+// set the folder `public` as folder containing static assets
+// such as css, js, and image files
+app.use(express.static('public'));
+
+// connects to the database
+db.connect();
+
+// use `express-session`` middleware and set its options
+// use `MongoStore` as server-side session storage
+app.use(session({
+    'secret': 'ccapdev-session',
+    'resave': false,
+    'saveUninitialized': false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+
+// define the paths contained in `./routes/routes.js`
+app.use('/', routes);
+
+// if the route is not defined in the server, render `../views/error.hbs`
+// always define this as the last middleware
+app.use(function (req, res) {
+
+    var details = {};
+
+    /*
+        checks if a user is logged in by checking the session data
+        if a user is logged-in,
+        display the profile tab and logout tab in the nav bar.
+    */
+    if(req.session.idNum) {
+        details.flag = true;
+        details.name = req.session.name;
+        details.idNum = req.session.idNum;
+    }
+
+    /*
+        if no user is logged-in,
+        do not display the profile tab and the logout tab in the nav bar.
+    */
+    else
+        details.flag = false;
+
+    // render `../views/error.hbs`
+    res.render('error', details);
+});
+
+// binds the server to a specific port
+app.listen(port, function () {
+    console.log('app listening at port ' + port);
+});
